8587c2c2267225c332f1137473cbb962ad0cae20
Enol <grimgaiden@gmail.com>
2020-06-29 19:36:20 +0200

    Added user POJO to learn about object Model

--- src/main/java/com/bolsadeideas/springboot/app/springbootweb/controllers/IndexController.java
+++ src/main/java/com/bolsadeideas/springboot/app/springbootweb/controllers/IndexController.java
@@ -1,5 +1,7 @@
 package com.bolsadeideas.springboot.app.springbootweb.controllers;
 
+import com.bolsadeideas.springboot.app.springbootweb.models.Usuario;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,5 +15,15 @@ public class IndexController {
     public String index(Model model) {
         model.addAttribute("titulo", "Hola Spring Framework");
         return "index";
+    }
+
+    @RequestMapping("/perfil")
+    public String perfil(Model model) {
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Andrés");
+        usuario.setApellido("Guzmán");
+        model.addAttribute("usuario", usuario);
+        model.addAttribute("titulo", "Perfil del usuario: ".concat(usuario.getNombre()));
+        return "perfil";
     }
 }
